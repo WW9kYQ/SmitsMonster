0a3790610e6a510171adc7aed80c4cc06d464839
Maxence HUET <maxence.huet2003@gmail.com>
2025-03-05 17:51:47 +0100

    Fix vue

--- src/main/java/fr/atlantique/imt/inf211/jobmngt/controller/PagesController.java
+++ src/main/java/fr/atlantique/imt/inf211/jobmngt/controller/PagesController.java
@@ -3,14 +3,19 @@ package fr.atlantique.imt.inf211.jobmngt.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PagesController {
 
 
 	@RequestMapping("/")
-	public String welcomePage(){
-        return "index.html";
+	public ModelAndView welcomePage(){
+		ModelAndView modelAndView = new ModelAndView("index");
+		modelAndView.addObject("countCompanies", 0);
+		modelAndView.addObject("countCandidates", 0);
+
+		return modelAndView;
         
 	}
 
